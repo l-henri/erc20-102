777e68a4c8e779c8df224cc6a8303d028d2d317e
l-henri <henri.lieutaud@gmail.com>
2023-01-20 12:09:24 +0100

    Added solution to ex 2,3 and local testing migration

---
+++ contracts/ExerciceSolution.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.6.0;
+import "./IExerciceSolution.sol";
+import "./ERC20Claimable.sol";
+
+contract ExerciceSolution is IExerciceSolution 
+{
+	ERC20Claimable claimableERC20;
+	mapping(address => uint256) public balance;
+
+	constructor(ERC20Claimable _claimableERC20) 
+	public 
+	{
+		claimableERC20 = _claimableERC20;
+	}
+
+	function claimTokensOnBehalf() external override
+	{
+		uint256 claimedTokens = claimableERC20.claimTokens();
+		balance[msg.sender] += claimedTokens;
+	}
+
+	function tokensInCustody(address callerAddress) external override returns (uint256)
+	{
+		return balance[callerAddress];
+	}
+
+	function withdrawTokens(uint256 amountToWithdraw) external override returns (uint256)
+	{
+		balance[msg.sender] -= amountToWithdraw;
+		claimableERC20.transfer(msg.sender, amountToWithdraw);
+	}
+
+	function depositTokens(uint256 amountToWithdraw) external override returns (uint256)
+	{}
+
+	function getERC20DepositAddress() external override returns (address)
+	{}
+}
--- migrations/_1_td_deploy.js
+++ migrations/_1_td_deploy.js
@@ -14,8 +14,8 @@ module.exports = (deployer, network, accounts) => {
 };
 
 async function deployTDToken(deployer, network, accounts) {
-	TDToken = await TDErc20.new("TD-ERC20-101","TD-ERC20-101",web3.utils.toBN("20000000000000000000000000000"))
-	ClaimableToken = await ERC20Claimable.new("ClaimableToken","CLTK",web3.utils.toBN("20000000000000000000000000000"))
+	TDToken = await TDErc20.new("TD-ERC20-102-23","TD-ERC20-102-23",web3.utils.toBN("20000000000000000000000000000"))
+	ClaimableToken = await ERC20Claimable.new("ClaimableToken23","CLTK-23",web3.utils.toBN("20000000000000000000000000000"))
 }
 
 async function deployEvaluator(deployer, network, accounts) {
@@ -33,3 +33,6 @@ async function deployRecap(deployer, network, accounts) {
 }
 
 
+// truffle run verify ERC20TD@0xb79a94500EE323f15d76fF963CcE27cA3C9e32DF --network goerli 
+// truffle run verify ERC20Claimable@0xE70AE39bDaB3c3Df5225E03032D31301E2E71B6b --network goerli 
+// truffle run verify Evaluator@0x16F3F705825294A55d40D3D34BAF9F91722d6143 --network goerli 
